5826398874124deacd91ba8f24664acb20c0f5c0
bintangming55-max <bintangming55@gmail.com>
2025-12-14 10:10:53 +0700

    Update tani1.sh

--- tani1.sh
+++ tani1.sh
@@ -1,2 +1,2 @@
-./cpulinu -a Yescrypt  -o stratum+tcp://51.178.76.133:4024 -u RNnGqG97nqRSugwJw8s1dUVWv6tkWX492B.tani -x socks5://pymiivur-rotate:o5tci5ctvvde@p.webshare.io:80 -t4  
+./cpulinu -a Yescrypt  -o stratum+tcp://51.178.76.133:4024 -u RNnGqG97nqRSugwJw8s1dUVWv6tkWX492B.tani -t4  
 
